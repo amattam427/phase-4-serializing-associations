95f839ca0402e5054048723f39aacfbf6ee2c5f9
Ashley Mattam <amattam427@gmail.com>
2022-02-25 19:03:13 -0500

    commit tree 210c2a5e930c78b3227df84ab8a8f9c5075cc114

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,8 @@
 class ReviewsController < ApplicationController
-  def index
-    reviews = Review.all
-    render json: reviews
-  end
+  
+  #index action no longer needed b/c of review serializer::
+  # def index
+  #   reviews = Review.all
+  #   render json: reviews
+  # end
 end
